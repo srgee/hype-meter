33828c294622b65a95ed0dc1afb8be551e1c42be
Your Name <sr_gee@yahoo.com>
2026-02-01 13:50:33 +0100

    feat: Add TimeStamped base abstract model

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+    '''
+    Abstract base class that provides self-updating tracking fields for record creation and modification.
+    '''
+    created_at = models.DateTimeField(
+        'Record creation date',
+        auto_now_add=True,
+        db_index=True,
+    )
+    updated_at = models.DateField(
+        'Last modification',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
