55527070a7f649ad405cb92466a40515f2b247a5
Your Name <sr_gee@yahoo.com>
2026-02-13 18:41:12 +0100

    fix: Downgrade to psycopg2-binary to solve import error

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
